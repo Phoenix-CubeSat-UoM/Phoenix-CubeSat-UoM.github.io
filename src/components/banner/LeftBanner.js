fefe859c0f84e1529eff6580f6d798e67cd81e20
Chathura Jayasinghe <jayasinghejmcnb.21@uom.lk>
2025-05-26 20:34:49 +0530

    reduce speed of text

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -10,26 +10,28 @@ const LeftBanner = () => {
       "Join us in this adventure!",
     ],
     loop: true,
-    typeSpeed: 20,
-    deleteSpeed: 10,
+    typeSpeed: 30,  
+    deleteSpeed: 20,   
     delaySpeed: 2000,
   });
   return (
     <div className="w-full lgl:w-1/2 flex flex-col gap-20">
-      <div className="flex flex-col gap-5">
-        <h1 className="text-6xl font-bold text-white">
+      <div className="flex flex-col gap-4 sm:gap-5">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white">
           Welcome to{" "}
           <span className="text-designColor capitalize">Project Phoenix</span>
         </h1>
-        <h2 className="text-4xl font-bold text-white mb-6">
+
+        <h2 className="text-lg sm:text-2xl md:text-3xl font-bold text-white">
           <span>{text}</span>
           <Cursor
-            cursorBlinking="false"
+            cursorBlinking={false}
             cursorStyle="|"
             cursorColor="#ff014f"
           />
         </h2>
-        <p className="text-base font-bodyFont leading-6 tracking-wide mt-6">
+
+        <p className="text-sm sm:text-base font-bodyFont leading-6 tracking-wide text-white">
           Project Phoenix is a collaboration between the Electronic Club and
           SEDS Mora of the University of Moratuwa. This project involves
           developing a Cube Satellite in two progressive stages. First, we
@@ -39,12 +41,13 @@ const LeftBanner = () => {
           development of the Cube Satellite.
         </p>
       </div>
-      <div className="flex flex-col items-center mt-8">
-        <p className="text-center text-lg mt-4">
+
+      <div className="flex flex-col items-center ">
+        <p className="text-center text-lg ">
           Join with PHOENIX to shape the world
         </p>
         <a
-          href="https://docs.google.com/forms/d/1pGWIpwzGru9a508gmzeRArRulS3okyA2JTPEKaNJ47k/edit?pli=1"
+          href="https://forms.gle/wFWKefrnEqvUogbP7"
           target="_blank"
           rel="noopener noreferrer"
           className="w-36 text-center px-6 py-3 bg-designColor text-white rounded-lg shadow-lg hover:bg-designColor transition duration-300 mt-4"
--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -6,7 +6,7 @@ const RightBanner = () => {
     <div className="w-full lgl:w-1/2 flex justify-center items-center">
       <div className="bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex justify-center items-center">
         <img
-          className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] rounded-2xl"
+          className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[550px] rounded-2xl"
           src={bannerImg}
           alt="bannerImg"
         />
